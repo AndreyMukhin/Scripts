3aa117329a671f99bd2bb12f865a21863d8d5d38
AndreyMukhin <ab_mukhin@mail.ru>
2017-03-02 21:35:43 +0300

    Download 7zip

---
+++ 7zip.ps1
@@ -0,0 +1,13 @@
+﻿$webClient = New-Object -TypeName System.Net.WebClient
+$7zipURL = "http://www.7-zip.org/a/7z1604-x64.exe"
+$7ZipExe = $env:TEMP + "\7zip.exe"
+
+try
+{
+    $webClient.DownloadFile($7zipURL, $7ZipExe)
+    & $7ZipExe
+}
+catch 
+{
+    Write-Error "7-zip was not installed!"
+}
