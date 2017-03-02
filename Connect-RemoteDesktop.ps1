b288692d4db04e70d2bb3275f45ddbb3f8aee2df
AndreyMukhin <ab_mukhin@mail.ru>
2017-03-02 21:28:11 +0300

    Connect to remote desktop

---
+++ Connect-RemoteDesktop.ps1
@@ -0,0 +1,11 @@
+﻿$machineName = "machineName"
+
+cd $env:windir\system32
+mstsc.exe
+Start-Sleep -Milliseconds 1000
+[System.Windows.Forms.SendKeys]::SendWait($machineName)
+[System.Windows.Forms.SendKeys]::SendWait("~")
+
+Start-Sleep -Milliseconds 5000
+[System.Windows.Forms.SendKeys]::SendWait("psw")
+[System.Windows.Forms.SendKeys]::SendWait("~")
