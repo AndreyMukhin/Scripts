e1aaa112af052ea381710e38cf1e4b945e1aa8b3
AndreyMukhin <ab_mukhin@mail.ru>
2017-03-03 16:07:07 +0300

    Add files via upload

---
+++ Notepad++.ps1
@@ -0,0 +1,13 @@
+﻿$webClient = New-Object -TypeName System.Net.WebClient
+$link = "https://notepad-plus-plus.org/repository/7.x/7.3.2/npp.7.3.2.Installer.x64.exe"
+$exe = $env:TEMP + "\npp.7.3.2.Installer.x64.exe"
+
+try
+{
+    $webClient.DownloadFile($link, $exe)
+    & $exe
+}
+catch 
+{
+    Write-Error "Notepad++ was not downloaded!"
+}
